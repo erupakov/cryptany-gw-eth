2f2a09335b54578885c13c2aea85e8ee73a78005
Eugene Rupakov <eugene.rupakov@gmail.com>
2017-10-27 11:21:11 +0300

    Initial commit

---
+++ api/routes/cgwRoutes.js
@@ -0,0 +1,11 @@
+﻿'use strict';
+
+module.exports = function(app) {
+	var contractAPI = require('../controllers/cgwController');
+
+  	// Routes
+	app.route('/sendMoney')
+    	.post(contractAPI.sendMoney);
+	app.route('/estimateGas')
+    	.post(contractAPI.estimateGas);
+};
---
+++ server.js
@@ -0,0 +1,14 @@
+﻿var express = require('express'),
+	app = express(),
+	port = process.env.PORT || 3000,
+	bodyParser = require('body-parser');
+  
+	app.use(bodyParser.urlencoded({ extended: true }));
+	app.use(bodyParser.json());
+
+	var routes = require('./api/routes/cgwRoutes'); //importing route
+	routes(app); //register the route
+
+	app.listen(port);
+
+console.log('CTPN (Cryptany Trusted Payment Network) Ethereum Gateway RESTful API server started on: ' + port);
